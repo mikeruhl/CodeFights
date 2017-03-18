55168f431dcc812eb9337e396f978c6ab2e793cd
Michael Ruhl <michael.ruhl@pnc.com>
2017-03-18 09:00:34 -0400

    pushing what I have so far to work with on another machine

---
+++ CodeFights.Tests/CodeFightsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace CodeFights.Tests
+{
+    [TestFixture]
+    public class CodeFightsTests
+    {
+
+        [TestCase(new[] { -1, 150, 190, 170, -1, -1, 160, 180 }, ExpectedResult = new[] {-1, 150, 160, 170, -1, -1, 180, 190 })]
+        [TestCase(new[] { -1, -1, -1, -1, -1 }, ExpectedResult = new[] { -1, -1, -1, -1, -1 })]
+        [TestCase(new[] { 4, 2, 9, 11, 2, 16 }, ExpectedResult = new[] { 2, 2, 4, 9, 11, 16 })]
+        public int[] TestsortByHeight(int[] a)
+        {
+            var cfr = new CodeFights();
+            return cfr.sortByHeight(a);
+        }
+
+        [TestCase(1230, ExpectedResult = true)]
+        [TestCase(239017, ExpectedResult = false)]
+        [TestCase(134008, ExpectedResult = true)]
+        public bool TestisLucky(int n)
+        {
+            var cfr = new CodeFights();
+            return cfr.isLucky(n);
+        }
+
+        [Test]
+        public void TestcommonCharacterCount()
+        {
+            var s1 = "aabcc";
+            var s2 = "adcaa";
+
+            var cfr = new CodeFights();
+
+            Assert.AreEqual(3, cfr.commonCharacterCount(s1, s2));
+        }
+        [Test]
+        public void TestmatrixElementsSum()
+        {
+            int[][] testone = new int[][] {new[] {0, 1, 1, 2}, new[] {0, 5, 0, 0}, new[] {2, 0, 3, 3}};
+            var testtwo = new int[][] { new[] { 1, 1, 1, 0 }, new[] { 0, 5, 0, 1 }, new[] { 2, 1, 3, 10 } };
+            var testthree = new int[][] { new[] { 1, 1, 1 }, new[] { 2, 2, 2 }, new[] { 3, 3, 3 } };
+            var testfour = new int[][] {new[] {0}};
+
+            var cfr = new CodeFights();
+
+            Assert.AreEqual(9, cfr.matrixElementsSum(testone));
+            Assert.AreEqual(9, cfr.matrixElementsSum(testtwo));
+            Assert.AreEqual(18, cfr.matrixElementsSum(testthree));
+            Assert.AreEqual(0, cfr.matrixElementsSum(testfour));
+        }
+    }
+}
---
+++ CodeFights/CodeFights.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights
+{
+    public class CodeFights
+    {
+        public string reverseParentheses(string s)
+        {
+            var startReversed = false;
+            var splits = s.Split('(', ')');
+            if (s.StartsWith("("))
+                startReversed = true;
+
+            var assembled = new List<string>();
+
+            for (var i = 0; i < splits.Length/2; i++)
+            {
+                assembled.Add(splits[i] + splits[i + splits.Length / 2]);
+            }
+
+            foreach (var word in assembled)
+            {
+                if()
+            }
+        }
+        public int[] sortByHeight(int[] a)
+        {
+            var output = new List<int>();
+            var lastSmall = int.MinValue;
+            foreach (var i in a)
+            {
+                if (i == -1)
+                {
+                    output.Add(i);
+                }
+                else
+                {
+                    if (a.Where(c => c == lastSmall).Count() > output.Count(d => d == lastSmall))
+                        lastSmall = a.Where(b => b != -1 && b >= lastSmall).Min();
+                    else
+                        lastSmall = a.Where(b => b != -1 && b > lastSmall).Min();
+                    output.Add(lastSmall);
+                }
+            }
+            return output.ToArray();
+        }
+
+        public bool isLucky(int n)
+        {
+            var a = n.ToString().ToCharArray();
+            var suma = 0;
+            var sumb = 0;
+
+            for (var i = 0; i < a.Length / 2; i++)
+            {
+                suma += int.Parse(a[i].ToString());
+                sumb += int.Parse(a[i + a.Length / 2].ToString());
+            }
+
+            return suma == sumb;
+        }
+
+        public int commonCharacterCount(string s1, string s2)
+        {
+            var a1 = s1.ToCharArray();
+            var a2 = s2.ToCharArray();
+
+            var count = 0;
+
+            foreach (var i in a1.Distinct())
+            {
+                count += Math.Min(a1.Where(b => b == i).Count(), a2.Where(b => b == i).Count());
+            }
+
+            return count;
+        }
+
+        string[] allLongestStrings(string[] inputArray)
+        {
+            var maxLen = int.MinValue;
+            foreach (var i in inputArray)
+            {
+                if (i.Length > maxLen)
+                    maxLen = i.Length;
+            }
+            return Array.FindAll(inputArray, x => x.Length == maxLen);
+
+        }
+
+        public int matrixElementsSum(int[][] matrix)
+        {
+            var ignoreInd = new List<int>();
+            var price = 0;
+
+            for (var x = 0; x < matrix.Length; x++)
+            {
+                for (var y = 0; y < matrix[x].Length; y++)
+                {
+                    if (ignoreInd.Any(a=>a==y))
+                        continue;
+
+                    if (matrix[x][y] == 0)
+                    {
+                        ignoreInd.Add(y);
+                    }
+                    price += matrix[x][y];
+                }
+            }
+
+            return price;
+        }
+    }
+}
