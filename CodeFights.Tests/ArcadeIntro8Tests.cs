47882ebfd888efafa8aa685bd0d6070fbd2f64c7
Michael Ruhl <michael.ruhl@pnc.com>
2017-03-21 11:26:55 -0400

    Arcade Intro Level 8 complete

---
+++ CodeFights.Tests/ArcadeIntro8Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeFights.Tests
+{
+    [Category("ArcadeLevel8")]
+    [TestFixture]
+    public class ArcadeIntro8Tests
+    {
+        [TestCase(new[] { 2, 3, 5, 1, 6 }, 2, ExpectedResult = 8, Description = "L8.4.1")]
+        [TestCase(new[] { 2, 4, 10, 1 }, 2, ExpectedResult = 14, Description = "L8.4.2")]
+        [TestCase(new[] { 1, 3, 2, 4 }, 3, ExpectedResult = 9, Description = "L8.4.3")]
+        [TestCase(new[] { 3, 2, 1, 1 }, 1, ExpectedResult = 3, Description = "L8.4.4")]
+        public int TestarrayMaxConsecutiveSum(int[] inputArray, int k)
+        {
+            return ArcadeIntro8.arrayMaxConsecutiveSum(inputArray, k);
+        }
+
+
+        [TestCase("cabca", ExpectedResult = 3, Description = "L8.3.1")]
+        [TestCase("aba", ExpectedResult = 2, Description = "L8.3.2")]
+        public int TestdifferentSymbolsNaive(string s)
+        {
+            return ArcadeIntro8.differentSymbolsNaive(s);
+        }
+
+        [TestCase("var_1__Int", ExpectedResult = '1', Description = "L8.2.1")]
+        [TestCase("q2q-q", ExpectedResult = '2', Description = "L8.2.2")]
+        [TestCase("0ss", ExpectedResult = '0', Description = "L8.2.3")]
+        public char TestfirstDigit(string inputString)
+        {
+            return ArcadeIntro8.firstDigit(inputString);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 3, ExpectedResult = new []{ 1, 2, 4, 5, 7, 8, 10}, Description = "L8.1.1")]
+        [TestCase(new[] { 1, 1, 1, 1, 1 }, 1, ExpectedResult = new int[] {}, Description = "L8.1.2")]
+        [TestCase(new[] { 1, 2, 1, 2, 1, 2, 1, 2 }, 2, ExpectedResult = new[] { 1, 1, 1, 1 }, Description = "L8.1.3")]
+        public int[] TestextractEachKth(int[] inputArray, int k)
+        {
+            return ArcadeIntro8.extractEachKth(inputArray, k);
+        }
+    }
+}
---
+++ CodeFights/ArcadeIntro8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights
+{
+    public static class ArcadeIntro8
+    {
+
+        public static int arrayMaxConsecutiveSum(int[] inputArray, int k)
+        {
+            var output = int.MinValue;
+            var temp = int.MinValue;
+            for (var i = 0; i < inputArray.Length - k+1; i++)
+            {
+                temp = inputArray[i];
+                for (var b = 1; b < k; b++)
+                    temp += inputArray[b+ i];
+
+                //temp = inputArray.Where((s, e) => e >= i && e < i + k).Sum();
+                if (temp > output)
+                    output = temp;
+            }
+
+            return output;
+        }
+
+
+        public static int differentSymbolsNaive(string s)
+        {
+            return s.ToCharArray().GroupBy(c => c).Count();
+        }
+
+        public static char firstDigit(string inputString)
+        {
+            return inputString.ToCharArray().First(c => "1234567890".Contains(c));
+        }
+
+        public static int[] extractEachKth(int[] inputArray, int k)
+        {
+            return inputArray.Where((t, i) => (i + 1) % k != 0).ToArray();
+        }
+    }
+}
