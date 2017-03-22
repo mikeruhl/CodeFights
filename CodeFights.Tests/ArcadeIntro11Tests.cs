21a1e697bb0fc9580c9e748003f06d4783a0cf33
Michael Ruhl <michael.ruhl@pnc.com>
2017-03-22 08:20:25 -0400

    Intro Level 11 done.

---
+++ CodeFights.Tests/ArcadeIntro11Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeFights.Tests
+{
+    [TestFixture]
+    public class ArcadeIntro11Tests
+    {
+        [TestCase(152, ExpectedResult = 52, Description = "L11.4.1")]
+        [TestCase(1001, ExpectedResult = 101, Description = "L11.4.2")]
+        [TestCase(10, ExpectedResult = 1, Description = "L11.4.3")]
+        public int TestdeleteDigit(int n)
+        {
+            return ArcadeIntro11.deleteDigit(n);
+        }
+
+        [TestCase("a1", ExpectedResult = 2, Description = "L11.3.1")]
+        [TestCase("c2", ExpectedResult = 6, Description = "L11.3.2")]
+        [TestCase("d4", ExpectedResult = 8, Description = "L11.3.3")]
+        [TestCase("g6", ExpectedResult = 6, Description = "L11.3.4")]
+        public int TestchessKnight(string cell)
+        {
+            return ArcadeIntro11.chessKnight(cell);
+        }
+
+        [TestCase("aabbbc", ExpectedResult = "2a3bc", Description = "L11.2.1")]
+        [TestCase("abbcabb", ExpectedResult = "a2bca2b", Description = "L11.2.2")]
+        [TestCase("abcd", ExpectedResult = "abcd", Description = "L11.2.3")]
+        public string TestlineEncoding(string s)
+        {
+            return ArcadeIntro11.lineEncoding(s);
+        }
+
+        [TestCase('9', ExpectedResult = true, Description = "L11.1.1")]
+        [TestCase('-', ExpectedResult = false, Description = "L11.1.2")]
+        [TestCase('0', ExpectedResult = true, Description = "L11.1.3")]
+        public bool TestisDigit(char symbol)
+        {
+            return ArcadeIntro11.isDigit(symbol);
+        }
+
+    }
+}
---
+++ CodeFights/ArcadeIntro11.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights
+{
+
+    public static class ArcadeIntro11
+    {
+
+        public static int deleteDigit(int n)
+        {
+            var o = n.ToString().ToCharArray();
+            var x = int.MinValue;
+            int temp;
+            for (var i = 0; i < o.Length; i++)
+            {
+                var what = new string(o.Where((a, b) => b != i).ToArray()).TrimStart('0');
+                if (string.IsNullOrEmpty(what))
+                    temp = 0;
+                else
+                    temp = int.Parse(what);
+
+                if (temp > x)
+                    x = temp;
+            }
+
+            return x;
+        }
+
+        public static int chessKnight(string cell)
+        {
+            var c = 0;
+            switch (cell.ToCharArray()[0])
+            {
+                case 'a':
+                    c = 1;
+                    break;
+                case 'b':
+                    c = 2;
+                    break;
+                case 'c':
+                    c = 3;
+                    break;
+                case 'd':
+                    c = 4;
+                    break;
+                case 'e':
+                    c = 5;
+                    break;
+                case 'f':
+                    c = 6;
+                    break;
+                case 'g':
+                    c = 7;
+                    break;
+                case 'h':
+                    c = 8;
+                    break;
+            }
+
+            var r = int.Parse(cell.ToCharArray()[1].ToString());
+            var tally = 0;
+
+            if (c + 1 < 9 & r + 2 < 9)
+                tally++;
+            if (c + 2 < 9 & r + 1 < 9)
+                tally++;
+            if (c + 2 < 9 & r - 1 > 0)
+                tally++;
+            if (c + 1 < 9 & r - 2 > 0)
+                tally++;
+            if (c - 1 > 0 & r - 2 > 0)
+                tally++;
+            if (c - 2 > 0 & r - 1 > 0)
+                tally++;
+            if (c - 2 > 0 & r + 1 < 9)
+                tally++;
+            if (c - 1 > 0 & r + 2 < 9)
+                tally++;
+
+            return tally;
+
+        }
+
+        public static string lineEncoding(string s)
+        {
+            var sChar = s.ToCharArray();
+            var count = 1;
+            var output = new StringBuilder();
+            for (var i = 0; i < sChar.Length; i++)
+            {
+                if (i + 1 == sChar.Length || sChar[i] != sChar[i + 1])
+                {
+                    if (count == 1)
+                    {
+                        output.Append(sChar[i]);
+                    }
+                    else
+                    {
+                        output.Append(count + sChar[i].ToString());
+                    }
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                }
+
+            }
+            return output.ToString();
+        }
+
+        public static bool isDigit(char symbol)
+        {
+            return "1234567890".Contains(symbol);
+        }
+    }
+}
