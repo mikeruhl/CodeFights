6ad21ad90b8b61033b9267cecf21176600e3f391
Mike Ruhl <zombittack@me.com>
2017-04-05 22:34:16 -0400

    Labyrinth not quite done, branched from master until I finish.

---
+++ CodeFights.Tests/TheCore/LabyrintNestedLoopsTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFights.Tests.Common;
+using CodeFights.TheCore;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CodeFights.Tests.TheCore
+{
+    [TestFixture]
+    public class LabyrintNestedLoopsTests
+    {
+        #region Labyrinth8 Test Cases
+
+        private static List<ComplexTest<string[], int>> Labyrinth8 = new List<ComplexTest<string[], int>>
+        {
+            new ComplexTest<string[], int>(new [] {"onomatopoeia",
+ "philosophical",
+ "provocatively",
+ "thesaurus"}, 20),
+            new ComplexTest<string[], int>(new [] {"phenomenon",
+ "remuneration",
+ "particularly",
+ "pronunciation"}, 62),
+            new ComplexTest<string[], int>(new [] {"africa",
+ "america",
+ "australia",
+ "antarctica"}, 62),
+            new ComplexTest<string[], int>(new [] {"eternal",
+ "texas",
+ "chainsaw",
+ "massacre"}, 4),
+            new ComplexTest<string[], int>(new [] {"anaesthetist",
+ "thatch",
+ "ethnics",
+ "sabulous"}, 0),
+            new ComplexTest<string[], int>(new [] {"crossword",
+ "square",
+ "formation",
+ "something"}, 6)
+        };
+        #endregion
+        [Description("Labyrinth8")]
+        [TestCaseSource("Labyrinth8")]
+        public void TestcrosswordFormation(ComplexTest<string[], int> test)
+        {
+            Assert.AreEqual(test.ExpectedResult, LabyrintNestedLoops.crosswordFormation(test.Input));
+        }
+
+        [TestCase(6, 4, ExpectedResult = 23, Description = "Labyrinth.7.1")]
+        [TestCase(30, 2, ExpectedResult = 65, Description = "Labyrinth.7.2")]
+        [TestCase(8, 6, ExpectedResult = 49, Description = "Labyrinth.7.3")]
+        [TestCase(16, 20, ExpectedResult = 333, Description = "Labyrinth.7.4")]
+        public int TestrectangleRotation(int a, int b)
+        {
+            return LabyrintNestedLoops.rectangleRotation(a, b);
+        }
+
+
+        [TestCase(9, ExpectedResult = new[] { 2, 2 }, Description = "Labyrinth.6.1")]
+        [TestCase(1, ExpectedResult = new[] { 0, 1 }, Description = "Labyrinth.6.2")]
+        [TestCase(2, ExpectedResult = new[] { 0, 2 }, Description = "Labyrinth.6.3")]
+        [TestCase(7, ExpectedResult = new[] { 2, 1 }, Description = "Labyrinth.6.4")]
+        [TestCase(500, ExpectedResult = new[] { 403, 1 }, Description = "Labyrinth.6.5")]
+        [TestCase(4, ExpectedResult = new[] { 0, 4 }, Description = "Labyrinth.6.6")]
+        public int[] TestweakNumbers(int n)
+        {
+            return LabyrintNestedLoops.weakNumbers(n);
+        }
+
+        [TestCase(10, 12, ExpectedResult = 2, Description = "Labyrinth.5.1")]
+        [TestCase(1, 9, ExpectedResult = 20, Description = "Labyrinth.5.2")]
+        [TestCase(13, 13, ExpectedResult = 0, Description = "Labyrinth.5.3")]
+        [TestCase(12, 108, ExpectedResult = 707, Description = "Labyrinth.5.4")]
+        [TestCase(239, 777, ExpectedResult = 6166, Description = "Labyrinth.5.5")]
+        [TestCase(1, 1000, ExpectedResult = 11435, Description = "Labyrinth.5.6")]
+        public int TestcomfortableNumbers(int L, int R)
+        {
+            return LabyrintNestedLoops.comfortableNumbers(L, R);
+        }
+
+        [TestCase(1, 5, ExpectedResult = 5, Description = "Labyrinth.4.1")]
+        [TestCase(21, 5, ExpectedResult = 22, Description = "Labyrinth.4.2")]
+        [TestCase(8, 4, ExpectedResult = 10, Description = "Labyrinth.4.3")]
+        [TestCase(21, 6, ExpectedResult = 23, Description = "Labyrinth.4.4")]
+        [TestCase(76, 250, ExpectedResult = 166, Description = "Labyrinth.4.5")]
+        [TestCase(80, 1000, ExpectedResult = 419, Description = "Labyrinth.4.6")]
+        public int TestpagesNumberingWithInk(int current, int numberOfDigits)
+        {
+            return LabyrintNestedLoops.pagesNumberingWithInk(current, numberOfDigits);
+        }
+
+
+        [TestCase(16, ExpectedResult = 9, Description = "Labyrinth.3.1")]
+        [TestCase(103, ExpectedResult = 4, Description = "Labyrinth.3.2")]
+        [TestCase(1, ExpectedResult = 2, Description = "Labyrinth.3.3")]
+        public int TestsquareDigitsSequence(int a0)
+        {
+            return LabyrintNestedLoops.squareDigitsSequence(a0);
+        }
+
+
+        [TestCase(9, ExpectedResult = 2, Description = "Labyrinth.2.1")]
+        [TestCase(8, ExpectedResult = 0, Description = "Labyrinth.2.2")]
+        [TestCase(15, ExpectedResult = 3, Description = "Labyrinth.2.3")]
+        public int TestisSumOfConsecutive2(int n)
+        {
+            return LabyrintNestedLoops.isSumOfConsecutive2(n);
+        }
+
+
+        [TestCase(125, ExpectedResult = true, Description = "Labyrinth.1.1")]
+        [TestCase(72, ExpectedResult = false, Description = "Labyrinth.1.2")]
+        [TestCase(100, ExpectedResult = true, Description = "Labyrinth.1.3")]
+        [TestCase(11, ExpectedResult = false, Description = "Labyrinth.1.4")]
+        [TestCase(324, ExpectedResult = true, Description = "Labyrinth.1.5")]
+        [TestCase(256, ExpectedResult = true, Description = "Labyrinth.1.6")]
+        [TestCase(119, ExpectedResult = false, Description = "Labyrinth.1.7")]
+        [TestCase(12, ExpectedResult = false, Description = "Labyrinth.1.8")]
+        [TestCase(1, ExpectedResult = true, Description = "Labyrinth.1.9")]
+        [TestCase(2, ExpectedResult = false, Description = "Labyrinth.1.10")]
+        [TestCase(343, ExpectedResult = true, Description = "Labyrinth.1.11")]
+        [TestCase(144, ExpectedResult = true, Description = "Labyrinth.1.12")]
+        [TestCase(289, ExpectedResult = true, Description = "Labyrinth.1.13")]
+        [TestCase(225, ExpectedResult = true, Description = "Labyrinth.1.14")]
+        [TestCase(35, ExpectedResult = false, Description = "Labyrinth.1.15")]
+        [TestCase(3, ExpectedResult = false, Description = "Labyrinth.1.16")]
+        public bool TetsisPower(int n)
+        {
+            return LabyrintNestedLoops.isPower(n);
+        }
+    }
+}
---
+++ CodeFights/TheCore/LabyrintNestedLoops.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.TheCore
+{
+    public static class LabyrintNestedLoops
+    {
+
+        public static int crosswordFormation(string[] words)
+        {
+            var success = 0;
+            //    x
+            // a    b
+            //    y
+            for (var x = 0; x < 4; x++)
+            {
+                for (var y = 0; y < 4; y++)
+                {
+                    for (var a = 0; a < 4; a++)
+                    {
+                        for (var b = 0; b < 4; b++)
+                        {
+                            if (a == b | a == x | a == y |
+                                b == x | b == y |
+                                x == y)
+                                continue;
+
+                            var topWord = words[x];
+                            var bottomWord = words[y];
+                            var leftWord = words[a];
+                            var rightWord = words[b];
+
+                            var yOffset = new[] {leftWord.Length, rightWord.Length}.Max();
+                            var xOffset = new[] {topWord.Length, bottomWord.Length}.Max();
+
+                            var topRight = new List<int[]>();
+                            for (var c1 = 0; c1 < topWord.Length; c1++)
+                            {
+                                for (var c2 = 0; c2 < rightWord.Length; c2++)
+                                {
+                                    if (topWord[c1] == rightWord[c2])
+                                        topRight.Add(new[] { c1, c2 });
+                                }
+                            }
+                            if (topRight.Count == 0)
+                                continue;
+
+                            var bottomRight = new List<int[]>();
+                            for (var c1 = 0; c1 < bottomWord.Length; c1++)
+                            {
+                                for (var c2 = 0; c2 < rightWord.Length; c2++)
+                                {
+                                    if (bottomWord[c1] == rightWord[c2])
+                                        bottomRight.Add(new[] { c1, c2 });
+                                }
+                            }
+                            if (bottomRight.Count == 0)
+                                continue;
+
+
+                            var bottomLeft = new List<int[]>();
+                            for (var c1 = 0; c1 < bottomWord.Length; c1++)
+                            {
+                                for (var c2 = 0; c2 < leftWord.Length; c2++)
+                                {
+                                    if (bottomWord[c1] == leftWord[c2])
+                                        bottomLeft.Add(new[] { c1, c2 });
+                                }
+                            }
+                            if (bottomLeft.Count == 0)
+                                continue;
+
+                            var topLeft = new List<int[]>();
+                            for (var c1 = 0; c1 < topWord.Length; c1++)
+                            {
+                                for (var c2 = 0; c2 < leftWord.Length; c2++)
+                                {
+                                    if (topWord[c1] == leftWord[c2])
+                                        topLeft.Add(new[] { c1, c2 });
+                                }
+                            }
+                            if (topLeft.Count == 0)
+                                continue;
+
+                            Debug.WriteLine("Currently there are {0} possibilities", (topLeft.Count * topRight.Count * bottomRight.Count * bottomLeft.Count));
+                            foreach (var tl in topLeft)
+                            {
+                                foreach (var tr in topRight)
+                                {
+                                    foreach (var br in bottomRight)
+                                    {
+                                        foreach (var bl in bottomLeft)
+                                        {
+                                            if (xOffset - topWord.Length + tl[0] == xOffset - bottomWord.Length + bl[0] &&
+                                                xOffset - topWord.Length + tr[0] == xOffset - bottomWord.Length + br[0] &&
+                                                yOffset - leftWord.Length + tl[1] == yOffset - rightWord.Length + tr[1] &&
+                                                yOffset - leftWord.Length + bl[1] == yOffset - rightWord.Length + br[1]
+                                                && tl[0] < tr[0] && tl[1] < bl[1])
+                                                success++;
+                                        }
+                                    }
+                                }
+                            }
+
+
+
+
+                        }
+                    }
+                }
+
+            }
+            return success;
+        }
+
+        public static int rectangleRotation(int a, int b)
+        {
+            var ret = 0;
+            for (var x = -a - b; x <= a + b; x++)
+            {
+                for (var y = -a - b; y <= a + b; y++)
+                {
+                    if (2 * (x - y) * (x - y) <= a * a & 2 * (x + y) * (x + y) <= b * b)
+                        ret++;
+                }
+            }
+            return ret;
+        }
+
+
+        public static int[] weakNumbers(int n)
+        {
+            //list of array going [number, divisors, weakness]
+            var numbers = new List<int[]>();
+            var tempDivisors = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                tempDivisors = 0;
+                //find divisors
+                for (var d = 1; d <= Math.Sqrt(i); d++)
+                {
+                    if (i % d != 0) continue;
+                    if (i / d == d)
+                        tempDivisors++;
+                    else
+                        tempDivisors += 2;
+                }
+
+                numbers.Add(new[] { i, tempDivisors, numbers.Count(a => a[1] > tempDivisors) });
+            }
+            var max = numbers.Max(b => b[2]);
+            return new[] { max, numbers.Count(c => c[2] == max) };
+        }
+
+        public static int comfortableNumbers(int L, int R)
+        {
+            var comfort = 0;
+
+            for (var a = L; a <= R; a++)
+            {
+                for (var b = a + 1; b <= R; b++)
+                {
+                    var sumOfDigitsA = a.ToString().ToCharArray().Select(n => int.Parse(n.ToString())).Sum();
+                    var sumOfDigitsB = b.ToString().ToCharArray().Select(n => int.Parse(n.ToString())).Sum();
+                    var lowerA = a - sumOfDigitsA;
+                    var upperA = a + sumOfDigitsA;
+                    var lowerB = b - sumOfDigitsB;
+                    var upperB = b + sumOfDigitsB;
+                    if (b >= lowerA & b <= upperA
+                        & a >= lowerB & a <= upperB)
+                        comfort++;
+                }
+            }
+
+            return comfort;
+        }
+
+        public static int pagesNumberingWithInk(int current, int numberOfDigits)
+        {
+            while (numberOfDigits >= 0)
+            {
+                numberOfDigits -= current.ToString().Length;
+                current++;
+            }
+
+            return current - 2;
+
+        }
+
+
+        public static int squareDigitsSequence(int a0)
+        {
+            var it = 1;
+            var results = new List<int>();
+            results.Add(a0);
+            while (results.GroupBy(b => b).All(c => c.Count() == 1))
+            {
+                results.Add(
+                    results[results.Count - 1].ToString().ToCharArray().Select(c => int.Parse(c.ToString()) * int.Parse(c.ToString())).Sum());
+                it++;
+
+            }
+
+            return it;
+        }
+
+
+        public static int isSumOfConsecutive2(int n)
+        {
+            var temp = 0;
+            var output = 0;
+            for (var start = 1; start <= n - 1; start++)
+            {
+                temp = start * 2 + 1;
+                for (var i = 2; temp <= n; i++)
+                {
+                    if (temp == n)
+                        output++;
+
+                    temp += start + i;
+                }
+            }
+
+            return output;
+        }
+
+
+        public static bool isPower(int n)
+        {
+            if (n <= 1)
+                return true;
+
+            for (var a = 2; a <= 350; a++)
+            {
+                for (var b = 2; b <= 350; b++)
+                {
+
+                    if (Math.Pow(a, b) == (double)n)
+                        return true;
+
+                    if (Math.Pow(a, b) > n)
+                        break;
+                }
+            }
+            return false;
+        }
+
+    }
+}
