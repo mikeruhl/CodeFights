f42bc6bdd2c38157c658634c4a9bb5d0e76e39cb
Michael Ruhl <michael.ruhl@pnc.com>
2017-04-22 20:27:55 -0400

    finished Core/Spring of Integration

---
+++ CodeFights.Tests/TheCore/SpringOfIntegrationTests.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFights.Tests.Common;
+using CodeFights.TheCore;
+using NUnit.Framework;
+
+namespace CodeFights.Tests.TheCore
+{
+    [TestFixture]
+    public class SpringOfIntegrationTests
+    {
+
+        #region SOI9TestCases
+
+        private static List<ComplexTest<int[], string[]>> SOI9 = new List<ComplexTest<int[], string[]>>
+        {
+            new ComplexTest<int[], string[]>
+            {
+                Input = new[] {1, 3},
+                ExpectedResult = new [] {"    *",
+                    "    *",
+                    "   ***",
+                    "  *****",
+                    " *******",
+                    "*********",
+                    "   ***"}
+            },
+            new ComplexTest<int[], string[]>
+            {
+                Input = new [] {2, 4},
+                ExpectedResult = new [] {"      *",
+                    "      *",
+                    "     ***",
+                    "    *****",
+                    "   *******",
+                    "  *********",
+                    " ***********",
+                    "   *******",
+                    "  *********",
+                    " ***********",
+                    "*************",
+                    "    *****",
+                    "    *****"}
+            },
+            new ComplexTest<int[], string[]>
+            {Input = new [] {4, 8},
+                ExpectedResult = new [] {"            *",
+                    "            *",
+                    "           ***",
+                    "          *****",
+                    "         *******",
+                    "        *********",
+                    "       ***********",
+                    "      *************",
+                    "     ***************",
+                    "    *****************",
+                    "   *******************",
+                    "         *******",
+                    "        *********",
+                    "       ***********",
+                    "      *************",
+                    "     ***************",
+                    "    *****************",
+                    "   *******************",
+                    "  *********************",
+                    "        *********",
+                    "       ***********",
+                    "      *************",
+                    "     ***************",
+                    "    *****************",
+                    "   *******************",
+                    "  *********************",
+                    " ***********************",
+                    "       ***********",
+                    "      *************",
+                    "     ***************",
+                    "    *****************",
+                    "   *******************",
+                    "  *********************",
+                    " ***********************",
+                    "*************************",
+                    "        *********",
+                    "        *********",
+                    "        *********",
+                    "        *********"}
+            }
+        };
+        #endregion
+        [TestCaseSource("SOI9")]
+        public void TestchristmasTree(ComplexTest<int[], string[]> test)
+        {
+            Assert.AreEqual(test.ExpectedResult, SpringOfIntegration.christmasTree(test.Input[0], test.Input[1]));
+        }
+
+
+        [TestCase(new[] { 1, 4, 2 }, new[] { 27, 18, 24 }, ExpectedResult = 3, Description = "SpringOfIntegration.8.1")]
+        [TestCase(new[] { 1, 4, 2 }, new[] { 5, 6, 2 }, ExpectedResult = 2, Description = "SpringOfIntegration.8.2")]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 1, 1 }, ExpectedResult = -1, Description = "SpringOfIntegration.8.3")]
+        [TestCase(new[] { 1, 1000 }, new[] { 23, 22 }, ExpectedResult = 2, Description = "SpringOfIntegration.8.4")]
+        public int TestrunnersMeetings(int[] startPosition, int[] speed)
+        {
+            return SpringOfIntegration.runnersMeetings(startPosition, speed);
+        }
+
+        [TestCase("Look at this example of a correct text", 5, 15, ExpectedResult = true, Description = "SpringOfIntegration.7.1")]
+        [TestCase("abc def ghi", 4, 10, ExpectedResult = false, Description = "SpringOfIntegration.7.2")]
+        [TestCase("a a a a a a a a", 1, 10, ExpectedResult = true, Description = "SpringOfIntegration.7.3")]
+        [TestCase("ab cd fg xyz", 1, 5, ExpectedResult = false, Description = "SpringOfIntegration.7.4")]
+        [TestCase("aa aa aaaaa aaaaa aaaaa", 6, 11, ExpectedResult = true, Description = "SpringOfIntegration.7.5")]
+        public bool TestbeautifulText(string inputString, int l, int r)
+        {
+            return SpringOfIntegration.beautifulText(inputString, l, r);
+        }
+
+
+        [TestCase("cabca", ExpectedResult = 3, Description = "SpringOfIntegration.6.1")]
+        [TestCase("aba", ExpectedResult = 2, Description = "SpringOfIntegration.6.2")]
+        [TestCase("ccccccccccc", ExpectedResult = 1, Description = "SpringOfIntegration.6.3")]
+        [TestCase("cabdcabdcabd", ExpectedResult = 4, Description = "SpringOfIntegration.6.C1")]
+        [TestCase("abcdefghijklmnopqrstuvwxyzabc", ExpectedResult = 26, Description = "SpringOfIntegration.6.C2")]
+        [TestCase("dogsfrankylikeshotdogsfrankylikes", ExpectedResult = 18, Description = "SpringOfIntegration.6.C3")]
+        public int TestcyclicString(string s)
+        {
+            return SpringOfIntegration.cyclicString(s);
+        }
+
+        #region SOI5
+        private static List<ComplexTest<object[], int>> SOI5 = new List<ComplexTest<object[], int>>
+        {
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"abc",
+                    "aaa",
+                    "aba",
+                    "bab"
+                }, "bbb"},
+                ExpectedResult = 2
+            },
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"aacccc",
+                        "bbcccc"},
+                    "abdddd" },
+                ExpectedResult = 0
+            },
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"a",
+                    "b",
+                    "c",
+                    "d",
+                    "e"
+                }, "c"},
+                ExpectedResult = 4
+            },
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"aa",
+                    "ab",
+                    "ba"
+                }, "bb"},
+                ExpectedResult = 1
+            },
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"a",
+                    "b",
+                    "c",
+                    "d",
+                    "e"
+                }, "f"},
+                ExpectedResult = 0
+            },
+            new ComplexTest<object[], int>
+            {
+                Input = new object[] {new[] {"aaa",
+                    "aaa"
+                }, "aaa"},
+                ExpectedResult = 1
+            }
+        };
+        #endregion
+        [TestCaseSource("SOI5")]
+        public void TeststringsCrossover(ComplexTest<object[], int> test)
+        {
+            var inputArray = (string[])test.Input[0];
+            var result = (string)test.Input[1];
+            Assert.AreEqual(test.ExpectedResult, SpringOfIntegration.stringsCrossover(inputArray, result));
+        }
+
+        [TestCase("*..*", "*.*", ExpectedResult = 5, Description = "SpringOfIntegration.4.1")]
+        [TestCase("*...*", "*.*", ExpectedResult = 5, Description = "SpringOfIntegration.4.2")]
+        [TestCase("*..*.*", "*.***", ExpectedResult = 9, Description = "SpringOfIntegration.4.3")]
+        [TestCase("*.*", "*.*", ExpectedResult = 4, Description = "SpringOfIntegration.4.4")]
+        [TestCase("*.**", "*.*", ExpectedResult = 5, Description = "SpringOfIntegration.4.5")]
+        public int Testcombs(string comb1, string comb2)
+        {
+            return SpringOfIntegration.combs(comb1, comb2);
+        }
+
+
+        #region SOI3
+        private static List<ComplexTest<int[][], bool>> SOI3 = new List<ComplexTest<int[][], bool>>
+        {
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,21}, new[] {1, 23}, new[] {1, 21}, new[] {0, 23}},
+                ExpectedResult = true
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,21}, new[] {1, 23}, new[] {1, 21}, new[] {1, 23}},
+                ExpectedResult = false
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}, new[] {1, 21}, new[] {1, 23}, new[] {0, 21}, new[] {1, 22}, new[] {0, 22}},
+                ExpectedResult = true
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}, new[] {1, 21}, new[] {1, 23}, new[] {0, 21}},
+                ExpectedResult = true
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}, new[] {1, 21}, new[] {1, 23}, new[] {0, 21}},
+                ExpectedResult = true
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}},
+                ExpectedResult = false
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}, new[] {1, 23}},
+                ExpectedResult = true
+            },
+            new ComplexTest<int[][], bool>()
+            {
+                Input = new [] {new[] {0,23}, new[] {1, 22}},
+                ExpectedResult = false
+            }
+        };
+        #endregion
+
+        [TestCaseSource("SOI3")]
+        public void TestpairOfShoes(ComplexTest<int[][], bool> test)
+        {
+            Assert.AreEqual(test.ExpectedResult, SpringOfIntegration.pairOfShoes(test.Input));
+        }
+
+
+        [TestCase(new[] { 3, 5, 2, 4, 5 }, ExpectedResult = new[] { -1, 3, -1, 2, 4 }, Description = "SpringOfIntegration.2.1")]
+        [TestCase(new[] { 2, 2, 1, 3, 4, 5, 5, 3 }, ExpectedResult = new[] { -1, -1, -1, 1, 3, 4, 4, 1 }, Description = "SpringOfIntegration.2.2")]
+        [TestCase(new[] { 3, 2, 1 }, ExpectedResult = new[] { -1, -1, -1 }, Description = "SpringOfIntegration.2.3")]
+        public int[] TestarrayPreviousLess(int[] items)
+        {
+            return SpringOfIntegration.arrayPreviousLess(items);
+        }
+
+
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, ExpectedResult = 186, Description = "SpringOfIntegration.1.1")]
+        [TestCase(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, ExpectedResult = 64, Description = "SpringOfIntegration.1.2")]
+        [TestCase(new[] { 3, 3, 5, 5 }, ExpectedResult = 60, Description = "SpringOfIntegration.1.3")]
+        public int TestarrayConversion(int[] inputArray)
+        {
+            return SpringOfIntegration.arrayConversion(inputArray);
+        }
+
+    }
+}
---
+++ CodeFights/TheCore/SpringOfIntegration.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.TheCore
+{
+    public static class SpringOfIntegration
+    {
+
+        //fileNaming is in ArcadeIntro12
+
+        public static string[] christmasTree(int levelNum, int levelHeight)
+        {
+            var sb = new List<string>();
+            var max = 5 + (levelHeight - 1) * 2 + (levelNum - 1) * 2;
+            var middle = max / 2 + max % 2;
+
+            for (var x = 0; x < 2; x++)
+            {
+                sb.Add(new string(' ', middle-1) + "*");
+            }
+            sb.Add(new string(' ', middle-2) + "***");
+            for (var i = 0; i < levelNum; i++)
+            {
+                for (var j = 0; j < levelHeight; j++)
+                {
+                    sb.Add(new string(' ', middle - j - i - 3) + new string('*', 5 + j * 2 + i*2));
+                }
+            }
+
+            var add = levelHeight % 2 == 0;
+            for (var i = 0; i < levelNum; i++)
+            {
+                sb.Add(new string(' ', max / 2 + max % 2 - levelHeight / 2 - levelHeight % 2 - (add ? 1 : 0)) 
+                    + new string('*', levelHeight + (add ? 1 : 0)));
+            }
+            return sb.ToArray();
+        }
+
+        public static int runnersMeetings(int[] startPosition, int[] speed)
+        {
+            var retVal = 1;
+            var len = startPosition.Length;
+            for (var x = 0; x < len; x++)
+            {
+                for (var y = x + 1; y < len; y++)
+                {
+                    var distDiff = startPosition[y] - startPosition[x];
+                    var speedDiff = speed[x] - speed[y];
+                       var sum = 0;
+                    if (speedDiff == 0 && distDiff != 0)
+                        continue;
+                    for (var z = 0; z < len; z++)
+                    {
+                        if (startPosition[z] * speedDiff + speed[z] * distDiff
+                            == startPosition[x] * speedDiff + speed[x] * distDiff)
+                        {
+                            sum++;
+                        }
+                    }
+                    if (sum > retVal)
+                    {
+                        retVal = sum;
+                    }
+                }
+            }
+            return retVal == 1 ? -1 : retVal;
+
+
+        }
+
+        public static bool beautifulText(string inputString, int l, int r)
+        {
+            var splits = inputString.Split(' ');
+
+            for (var len = l; len <= r; len++)
+            {
+                var tot = 0;
+                foreach (var s in splits)
+                {
+                    if (tot > 0)
+                        tot += s.Length + 1;
+                    else
+                        tot += s.Length;
+
+                    if (tot > r)
+                        break;
+
+                    if (tot == len)
+                        tot = 0;
+                }
+                if(tot == 0)
+                return true;
+            }
+            return false;
+        }
+
+
+        public static int cyclicString(string s)
+        {
+            var b = string.Empty;
+            
+           for(var len = 1; len <= s.Length; len++)
+           {
+               var repeats = true;
+                b= s.Substring(0, len);
+                for (var i = b.Length; i < s.Length; i++)
+                {
+                    if (b[i % b.Length] != s[i])
+                    {
+                        repeats = false;
+                        break;
+                    }
+                }
+               if (repeats)
+                   break;
+           }
+            return b.Length;
+        }
+
+        public static int stringsCrossover(string[] inputArray, string result)
+        {
+            var poss = 0;
+            for (var x = 0; x < inputArray.Length - 1; x++)
+            {
+                var a = inputArray[x];
+                for (var y = x + 1; y < inputArray.Length; y++)
+                {
+                    var b = inputArray[y];
+                    var nope = false;
+                    for (var c = 0; c < result.Length; c++)
+                    {
+
+                        if (a[c] != result[c] && b[c] != result[c])
+                        {
+                            nope = true;
+                            break;
+                        }
+                    }
+                    if (!nope)
+                        poss++;
+                }
+
+            }
+            return poss;
+        }
+
+        public static int combs(string comb1, string comb2)
+        {
+            var smGap = comb1.Length + comb2.Length;
+            var smallest = smGap;
+            var offSet = 1;
+            var comb2Pad = new string('.', comb1.Length) + comb2 + new string('.', comb1.Length);
+            while (offSet != smGap)
+            {
+                var fit = true;
+                var comb1Pad = new string('.', offSet) + comb1;
+                for (var i = 0; i < comb1Pad.Length; i++)
+                {
+                    if (comb1Pad[i] == '*' & comb2Pad[i] == '*')
+                    {
+                        fit = false;
+                        break;
+                    }
+                }
+                if (fit)
+                {
+                    var totalLength = smGap;
+                    if (comb1Pad.Length > smGap)
+                    {
+                        totalLength = comb2.Length + comb1Pad.Length - smGap;
+                    }
+                    else
+                    {
+                        totalLength = comb2.Length + comb1.Length - offSet;
+                    }
+                    totalLength = Math.Max(comb1.Length, Math.Max(comb2.Length, totalLength));
+                    if (smallest > totalLength)
+                        smallest = totalLength;
+                }
+                offSet++;
+            }
+
+            return smallest;
+
+        }
+
+
+        public static bool pairOfShoes(int[][] shoes)
+        {
+            var pairs = shoes.GroupBy(f => f[1]);
+            return pairs.All(p => p.Count(f1 => f1[0] == 0) == p.Count(f1 => f1[0] == 1));
+        }
+
+
+        public static int[] arrayPreviousLess(int[] items)
+        {
+            var output = new int[items.Length];
+            output = output.Select(n => -1).ToArray();
+
+            for (var i = 0; i < items.Length; i++)
+            {
+                for (var y = i - 1; y >= 0; y--)
+                {
+                    if (items[y] < items[i])
+                    {
+                        output[i] = items[y];
+                        break;
+                    }
+                }
+            }
+            return output;
+        }
+
+
+        public static int arrayConversion(int[] inputArray)
+        {
+            var totalOut = 1;
+            var scratchList = new List<int>(inputArray);
+            var outList = new List<int>();
+
+            while (outList.Count != 1)
+            {
+                outList = new List<int>();
+                for (var i = 0; i < scratchList.Count; i += 2)
+                {
+                    if (i + 1 == scratchList.Count)
+                        outList.Add(scratchList[i]);
+                    else if (totalOut % 2 == 0)
+                    {
+                        outList.Add(scratchList[i] * scratchList[i + 1]);
+                    }
+                    else
+                    {
+                        outList.Add(scratchList[i] + scratchList[i + 1]);
+                    }
+                }
+
+                scratchList = outList;
+                totalOut++;
+            }
+
+            return outList[0];
+        }
+
+    }
+}
