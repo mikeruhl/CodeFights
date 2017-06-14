c1be99539dd2f10f686d0aa71a8f815ed9da0d75
Mike Ruhl <zombittack@me.com>
2017-06-13 22:17:23 -0400

    added SecretArchives

---
+++ CodeFights/TheCore/SecretArchives.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.TheCore
+{
+    public static class SecretArchives
+    {
+        public static string[] lrc2subRip(string[] lrcLyrics, string songLength)
+        {
+            var output = new List<string>();
+
+            for(var i = 0; i < lrcLyrics.Length; i++)
+            {
+
+            }
+        }
+    }
+}
