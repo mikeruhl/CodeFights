7a9e78c146e6ecf11c3cb49c9b8e90497ef3963a
Mike Ruhl <mike.ruhl@tobiidynavox.com>
2022-03-16 13:15:16 -0400

    findProfession

---
+++ CodeFights.Tests/InterviewPrep/TreesBasicTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeFights.InterviewPrep;
+using NUnit.Framework;
+
+namespace CodeFights.Tests.InterviewPrep
+{
+    [TestFixture]
+    public class TreesBasicTests
+    {
+        [TestCase(3, 3, ExpectedResult = "Doctor", Description = "TreesBasic.FindProfession.1")]
+        [TestCase(1, 1, ExpectedResult = "Engineer", Description = "TreesBasic.FindProfession.2")]
+        [TestCase(4, 5, ExpectedResult = "Doctor", Description = "TreesBasic.FindProfession.3")]
+        [TestCase(4, 6, ExpectedResult = "Engineer", Description = "TreesBasic.FindProfession.4")]
+        [TestCase(4, 2, ExpectedResult = "Doctor", Description = "TreesBasic.FindProfession.5")]
+        [TestCase(8, 100, ExpectedResult = "Engineer", Description = "TreesBasic.FindProfession.6")]
+        [TestCase(10, 470, ExpectedResult = "Engineer", Description = "TreesBasic.FindProfession.7")]
+        [TestCase(17, 5921, ExpectedResult = "Doctor", Description = "TreesBasic.FindProfession.8")]
+        [TestCase(20, 171971, ExpectedResult = "Engineer", Description = "TreesBasic.FindProfession.9")]
+        public string TestFindProfessions(int level, int position)
+        {
+            return FindProfession.Solution(level, position);
+        }
+    }
+}
---
+++ CodeFights/InterviewPrep/FindProfession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFights.InterviewPrep
+{
+    public static class FindProfession
+    {
+        public static string Solution(int level, int pos)
+        {
+            return findProfession(level,pos) == 'e' ? "Engineer" : "Doctor";
+        }
+
+        private static char findProfession(int level, int pos)
+        {
+            if (level == 1)
+                return 'e';
+
+            if (findProfession(level - 1,
+                              (pos + 1) / 2) == 'd')
+                return (pos % 2 > 0) ?
+                                 'd' : 'e';
+
+
+            return (pos % 2 > 0) ?
+                             'e' : 'd';
+        }
+    }
+}
